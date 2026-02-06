2e4b460b99042b99335ec3e17466d346f90b6366
Harsh Raj <harshraj.contact@gmail.com>
2026-02-06 22:18:12 +0530

    Initial Commit: Project Structure, Test Data, and main.c

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    printf("================================\n");
+    printf("Internship Application Validator\n");
+    printf("================================\n");
+    printf("\nProgram starting...\n");
+    
+    // TODO: Add file validation logic
+    
+    printf("\nProgram completed.\n");
+    return 0;
+}
